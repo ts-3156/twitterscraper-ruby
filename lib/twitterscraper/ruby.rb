9db70fa5bb9c31815622d0b62b0115949904c45c
ts-3156 <ts_3156@yahoo.co.jp>
2020-07-12 05:03:04 +0900

    Initial commit

---
+++ lib/twitterscraper/ruby.rb
@@ -0,0 +1,8 @@
+require "twitterscraper/ruby/version"
+
+module Twitterscraper
+  module Ruby
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/twitterscraper/ruby/version.rb
@@ -0,0 +1,5 @@
+module Twitterscraper
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
